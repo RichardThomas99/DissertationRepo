1180e122838c56681733362d897d3dac5ed1457e
RichardThomas99 <richard120thomas@hotmail.co.uk>
2020-02-24 15:37:33 +0000

    Added search bar testing file

---
+++ src/testScripts/searchBar.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from '../components/SearchBar';
+
+it("Search Bar Renders",()=>{
+  const div = document.createElement("div");
+  ReactDOM.render(<div></div>,div)
+})
