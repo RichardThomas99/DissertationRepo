efe86d0e27e4521edd812bebff5da7d2a014ba6c
RichardThomas99 <richard120thomas@hotmail.co.uk>
2020-02-24 14:59:43 +0000

    Changes to the app.css

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,13 +5,14 @@ function SearchBar()
 {
 
   return(
-    <form>
-    <label>
-      <input id = "textSearch"  type="text" name="product" />
-      </label>
-      <input type="submit" value="Search" />
-
-      </form>
+    <div class = "searchDiv">
+      <form>
+        <label>
+          <input id = "textSearch"  type="text" name="product" />
+        </label>
+          <input type="submit" value="Search" />
+        </form>
+      </div>
     );
 }
 
