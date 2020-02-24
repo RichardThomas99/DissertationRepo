666f977f36aacdefc9ab9896c14471f1e2a37d10
RichardThomas99 <richard120thomas@hotmail.co.uk>
2020-02-24 17:23:06 +0000

    Decay Rate Component infastructure implemented

--- src/components/DecayRate.js
+++ src/components/DecayRate.js
@@ -18,7 +18,7 @@ class DecayRate extends Component
     <div>
 
     <h3>DecayRate: {decayRate} </h3>
-    <p>The decay rate is a description of how quickly the average price of the trainer is changing over time.If the rate is between 0 and 1 the price is falling over time. If the rate is greater than 1 then the price is increasing</p>
+    <p>The decay rate is a description of how quickly the average price of the trainer is changing over time.If the rate is between 0 and 1 the price is falling over time. If the rate is greater than 1 then the price is increasing.</p>
     <p>Settings behind the decay-rate are listed below. </p>
       <ul id="content-list">
           <li>Original Array Used = Untampered</li>
