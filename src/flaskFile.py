7d05df7a07a8e23b0944b838fb2ecce452833641
RichardThomas99 <richard120thomas@hotmail.co.uk>
2020-03-02 17:39:41 +0000

    Obtaining JSON from WebScraper

--- src/flaskFile.py
+++ src/flaskFile.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def osStuff():
-    cmd = 'date'
-    os.system(cmd)
 
+    cmd = r"C:\Users\Richard\AppData\Roaming\SysNucleus\WebHarvy\WebHarvy.exe C:\Users\Richard\Documents\DissertationRepo\DissertationRepo\src\scraperConfig\depopstars5-works.xml 1  C:\Users\Richard\Documents\DissertationRepo\DissertationRepo\src\data\data.json"
+
+    os.system(cmd)
     return "os System Run"
