4b6df4690c1ca375a277acfb4bd5599eb17747c5
RichardThomas99 <richard120thomas@hotmail.co.uk>
2020-03-27 17:54:04 +0000

    Added graph aspect to listedTime over time component

--- src/components/ListedTimeOverTime.js
+++ src/components/ListedTimeOverTime.js
@@ -1,8 +1,49 @@
 import React, {Component} from 'react';
 import * as firebase from 'firebase';
+import {
+  BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
 
 class ListedTimeOverTime extends Component
 {
+  graph(listedTimeArray,dateArray)
+  {
+
+    var dataArray = Array(25);
+
+    for(var i=0;i<25;i++)
+    {
+      dataArray[i] =
+      {
+        Date:dateArray[i],
+        ListedAverage:listedTimeArray[i],
+      }
+    }
+
+
+    const renderBarChart = (
+
+      <BarChart
+            width={1500}
+            height={350}
+            data={dataArray}
+            margin={{
+              top: 5, right: 30, left: 20, bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="Date" />
+            <YAxis yAxisId="left" orientation="left" stroke="#8884d8" />
+            <Tooltip />
+            <Legend verticalAlign="top" wrapperStyle={{ lineHeight: '40px' }} />
+
+            <Bar yAxisId="left" dataKey="ListedAverage" fill="#8884d8" />
+          </BarChart>
+    );
+
+      return renderBarChart;
+
+  }
 
   listedTimeArray(item)
   {
@@ -53,7 +94,7 @@ class ListedTimeOverTime extends Component
       });
       console.log("date Array = " + dateArray);
       console.log("Listed Array = " + listedTimeArray );
-    return listedTimeArray;
+    return [listedTimeArray,dateArray];
   }
   render()
   {
@@ -77,6 +118,7 @@ class ListedTimeOverTime extends Component
     <p>The decay rate is a description of how quickly the average price of the trainer is changing over time.If the rate is between 0 and 1 the price is falling over time. If the rate is greater than 1 then the price is increasing.</p>
     <p>Settings behind the decay-rate are listed below. </p>
     {listedTimeArray}
+    {this.graph(listedTimeArray[0],listedTimeArray[1])}
       <ul id="content-list">
           <li>Original Array Used = Untampered</li>
           <li>Lower bounds = £{lowerBound}</li>
